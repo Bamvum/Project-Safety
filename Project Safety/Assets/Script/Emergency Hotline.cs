2ddab22a3b8466b7dead4b936a6860e89831ed97
Bamvum <135228716+Bamvum@users.noreply.github.com>
2024-07-11 16:36:06 +0800

    Emergency Hotline Feature

--- Project Safety/Assets/Script/Emergency Hotline.cs
+++ Project Safety/Assets/Script/Emergency Hotline.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Cinemachine;
+using Synty.AnimationBaseLocomotion.Samples;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class EmergencyHotline : MonoBehaviour
 {
@@ -20,6 +23,9 @@ public class EmergencyHotline : MonoBehaviour
     [Header("HUD")]
     [SerializeField] GameObject playerHUD;
     [SerializeField] GameObject contactHUD;
+    [Space(10)]
+    [SerializeField] GameObject phone;
+    bool contactMode;
     
     // [Space(10)]
 
@@ -30,11 +36,52 @@ public class EmergencyHotline : MonoBehaviour
 
     void OnEnable()
     {
+        playerControls.Contact.Contact1.performed += AccessContact1;
         playerControls.Contact.Enable();
+    }
+
+    private void AccessContact1(InputAction.CallbackContext context)
+    {
+        if(contactMode)
+        {
+
+        }
     }
 
     void OnDisable()
     {
         playerControls.Contact.Disable();
     }
+
+    public void PhoneTrigger()
+    {
+        // HUD
+        playerHUD.SetActive(false);
+        // contactHUD.SetActive(true);
+
+        // ANIMATION
+        playerMovement.playerAnim.SetBool("Idle", true);
+        playerMovement.playerAnim.SetBool("Phone", true);
+
+        //Cinemachine
+        playerVC.Priority = 0;
+        phoneVC.Priority = 10;
+
+        phone.SetActive(true);
+
+
+    }
+
+    void EndOfPhoneCall()
+    {
+        playerMovement.playerAnim.SetBool("Idle", false);
+        playerMovement.playerAnim.SetBool("Phone", false);
+
+        playerMovement.enabled = true;
+        interact.enabled = true;
+        stamina.enabled = true;
+        
+    }
+
+    
 }
